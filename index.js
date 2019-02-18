ebad7b5ee00965448ddaabfc40fe4b1b6c9d3e7e
Veniel Dorestave <veniel@noukod.com>
2019-02-18 21:27:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,29 @@
 // function fetchSpecifiedDrivers(driverArray, fn) {
 //     return fn(driverArray);
 // }
+
+
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+function createFareMultiplier(number){
+    return function (fare){return number * fare};
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, cb){
+    return cb(drivers);
+}
