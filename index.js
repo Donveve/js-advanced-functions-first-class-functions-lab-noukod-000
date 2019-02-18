9000cb292adbdd699dd06d69928a34cf4ae36140
Veniel Dorestave <veniel@noukod.com>
2019-02-18 21:27:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const returnFirstTwoDrivers = function(array) {
-    return array.slice(0, 2);
-};
+// const returnFirstTwoDrivers = function(array) {
+//     return array.slice(0, 2);
+// };
 
-const returnLastTwoDrivers = function(array) {
-    return array.slice(-2);
-};
+// const returnLastTwoDrivers = function(array) {
+//     return array.slice(-2);
+// };
 
-const selectingDrivers = [
-    returnFirstTwoDrivers, returnLastTwoDrivers
-];
+// const selectingDrivers = [
+//     returnFirstTwoDrivers, returnLastTwoDrivers
+// ];
 
-const createFareMultiplier = function(int) {
-    return function(arg) {
-        return int * arg;
-    };
-};
+// const createFareMultiplier = function(int) {
+//     return function(arg) {
+//         return int * arg;
+//     };
+// };
 
-const fareDoubler = createFareMultiplier(2);
+// const fareDoubler = createFareMultiplier(2);
 
-const fareTripler = createFareMultiplier(3);
+// const fareTripler = createFareMultiplier(3);
 
-function fetchSpecifiedDrivers(driverArray, fn) {
-    return fn(driverArray);
-}
+// function fetchSpecifiedDrivers(driverArray, fn) {
+//     return fn(driverArray);
+// }
